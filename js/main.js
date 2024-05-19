3977dd5a9d621773772c22961413cc9d24824fb2
Rafael Alexander Tena Chaparro <rafaelalexandertenachaparro@Rafael.local>
2024-05-18 23:05:54 -0600

    animation

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const main = document.querySelector("main");
+const mainHeader = document.querySelector(".main-header");
 const sidemenu = document.querySelector(".sidemenu");
 
 const sidemenu_nav = document.querySelector(".sidemenu-nav__options");
@@ -10,16 +11,21 @@ const isMobile = window.matchMedia(
 
 const options_nav = [
   {
-    label: "Calculadora ISR",
+    label: "Calculadora de ISR",
     link: "./index.html",
   },
   {
-    label: "Calculadora de aguinaldo",
+    label: "Example page 2",
+    link: "./index.html",
+  },
+  {
+    label: "Example page 3",
     link: "./index.html",
   },
 ];
 
 document.addEventListener("DOMContentLoaded", () => {
+  // =========== ADD NAVIGATION OPTIONS LIST =========== //
   options_nav.forEach((option) => {
     const option_nav = document.createElement("li");
     option_nav.innerHTML = `
@@ -37,21 +43,46 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const current_nav = document.querySelector(`a[href="./${current_url_page}"]`);
 
+  // =========== ADD ACTIVE CLASS TO CURRENT PAGE =========== //
+  if (current_url === "/") {
+    sidemenu_nav.querySelector("a").classList.add("active");
+  }
+
   if (current_nav) {
     current_nav.classList.add("active");
   }
+
+  // =========== ADD A RANDOM USER =========== //
+  const getRandomUser = async () => {
+    const response = await fetch("https://randomuser.me/api/");
+    const data = await response.json();
+    return data.results[0];
+  };
+
+  getRandomUser().then((user) => {
+    const userPictureUrl = user?.picture?.medium
+
+    const userImg = document.createElement("img");
+    userImg.src = userPictureUrl;
+    userImg.alt = "User picture";
+    userImg.classList.add("user-picture");
+
+    mainHeader.appendChild(userImg);
+  });
 });
 
 sidemenu_toggle.forEach((toggle) => {
   toggle.addEventListener("click", () => {
     sidemenu.classList.toggle("active");
     main.classList.toggle("full");
+
     sidemenu_toggle.forEach((toggle) => {
       toggle.classList.toggle("active");
     });
   });
 });
 
+// =========== CLOSE SIDEMENU ON CLICK OUTSIDE =========== //
 document.addEventListener("click", (e) => {
   if (
     sidemenu_toggle[0].contains(e.target) ||
